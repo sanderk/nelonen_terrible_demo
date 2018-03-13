b7501748e60a0d650181aedcd1e8f20e93f052ff
Sander Kieft <sander.kieft@sanomamedia.nl>
2018-03-13 13:49:17 +0200

    Bumped the volume size to 17G

--- deploy/terraform/test-sanderk/main.tf
+++ deploy/terraform/test-sanderk/main.tf
@@ -6,6 +6,7 @@ module "web_server" {
   name   = "webserver"
   tags   = "${var.tags}"
   count  = "${var.instance_count}"
+  volume_size = "17G"
   vpc_id = "vpc-0e942268"
   image  = "centos7-201801241238"
 }
