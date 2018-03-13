44412285847b435cf8e21cf45ec7b91d883115a2
Qing Jin <qing.jin@sanoma.com>
2018-03-13 11:40:46 +0200

    directory is changed for the basic

---
+++ deploy/terraform/test-qjin1/main.tf
@@ -0,0 +1,7 @@
+###hola Qing
+
+module "web_server" {
+  source          = "git::ssh://git@source.sanoma.com:7999/tfmod/ec2.git?ref=master"
+  name            = "webserver"
+  tags            = "${var.tags}"
+}
