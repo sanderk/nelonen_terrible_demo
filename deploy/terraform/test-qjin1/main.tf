554251de95c8ee08869d4a196fe976c88b845c16
Qing Jin <qing.jin@sanoma.com>
2018-03-13 13:43:50 +0200

    more content and source change to main.tf file

--- deploy/terraform/test-qjin1/main.tf
+++ deploy/terraform/test-qjin1/main.tf
@@ -1,7 +1,7 @@
 ###hola Qing
 
 module "web_server" {
-  source          = "git::ssh://git@source.sanoma.com:7999/tfmod/ec2.git?ref=master"
+  source          = "git::ssh://git@source.sanoma.com:7999/tfmod/ec2.git?ref=feature/multiaccount"
   name            = "webserver"
   tags            = "${var.tags}"
   count  = "${var.instance_count}"
