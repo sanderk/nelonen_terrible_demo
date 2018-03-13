a72cf59ef18e9ce3135dbfca5d1bd5048a372ea4
Sebastiaan van der Vliet <sebastiaan.vandervliet@sanoma.com>
2018-03-13 11:55:31 +0100

    debugging

--- deploy/terraform/test-sanderk/variables.tf
+++ deploy/terraform/test-sanderk/variables.tf
@@ -2,11 +2,7 @@
 # My First Variables
 #
 variable "instance_count" {
-    description: "The Number of Instances"
+    description = "The Number of Instances"
     value: 3
 }
 
-variable "timezone" {
-    description: "The Number of Instances"
-    value: "Europe/Helsinki"
-}
