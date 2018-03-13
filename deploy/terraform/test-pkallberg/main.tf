9a7080cc0a0d3eb6beaadea17dad1743706aef9f
Petri Kallberg <petri.kallberg@sanoma.com>
2018-03-13 13:42:36 +0200

    using multiaccount version of ec2 module

--- deploy/terraform/test-pkallberg/main.tf
+++ deploy/terraform/test-pkallberg/main.tf
@@ -2,7 +2,7 @@
 # Hello World Web Server
 #
 module "web_server" {
-  source = "git::ssh://git@source.sanoma.com:7999/tfmod/ec2.git?ref=master"
+  source = "git::ssh://git@source.sanoma.com:7999/tfmod/ec2.git?ref=feature/multiaccount"
   name   = "webserver"
   tags   = "${var.tags}"
   count  = "${var.instance_count}"
