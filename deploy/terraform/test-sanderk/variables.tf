f23cda41bd753ba481f1a046bbb4bf050f7eea26
Sander Kieft <sander.kieft@sanomamedia.nl>
2018-03-13 13:28:58 +0200

    Fixed parse error

--- deploy/terraform/test-sanderk/variables.tf
+++ deploy/terraform/test-sanderk/variables.tf
@@ -2,11 +2,11 @@
 # My First Variables
 #
 variable "instance_count" {
-    description: "The Number of Instances"
-    value: 3
+    description =  "The Number of Instances"
+    value = 3
 }
 
 variable "timezone" {
-    description: "The Number of Instances"
-    value: "Europe/Helsinki"
+    description = "The Number of Instances"
+    value = "Europe/Helsinki"
 }
