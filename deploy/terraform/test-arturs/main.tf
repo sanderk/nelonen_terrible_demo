e422a9d5815af037cf24d263b5332412060d99c1
Arturs Liepins <arturs@liepins.me>
2018-03-13 13:42:03 +0200

    Fix

--- deploy/terraform/test-arturs/main.tf
+++ deploy/terraform/test-arturs/main.tf
@@ -1,5 +1,5 @@
 module "web_server" {
-    source          = "git::ssh://git@source.sanoma.com:7999/tfmod/ec2.git?ref=master"
+    source          = "git::ssh://git@source.sanoma.com:7999/tfmod/ec2.git?ref=feature/multiaccount"
     name            = "webserver"
     tags            = "${var.tags}"
     count           = "${var.instance_count}"
