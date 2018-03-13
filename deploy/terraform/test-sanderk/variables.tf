17c21279834b75805a1661a9f9f75c46f3a5985d
Sander Kieft <sander.kieft@sanomamedia.nl>
2018-03-13 11:22:52 +0200

    Added the basic setup

---
+++ deploy/terraform/test-sanderk/variables.tf
@@ -0,0 +1,12 @@
+#
+# My First Variables
+#
+variable "instance_count" {
+    description: "The Number of Instances"
+    value: 3
+}
+
+variable "timezone" {
+    description: "The Number of Instances"
+    value: "Europe/Helsinki"
+}
