eb0e1f093479fd587518d5dc61d0b5a3ba463eb0
Qing Jin <qing.jin@sanoma.com>
2018-03-13 13:41:55 +0200

    more content to main.tf file

--- deploy/terraform/test-qjin1/main.tf
+++ deploy/terraform/test-qjin1/main.tf
@@ -4,4 +4,7 @@ module "web_server" {
   source          = "git::ssh://git@source.sanoma.com:7999/tfmod/ec2.git?ref=master"
   name            = "webserver"
   tags            = "${var.tags}"
+  count  = "${var.instance_count}"
+  vpc_id = "vpc-0e942268"
+  image  = "centos7-201801241238"
 }
