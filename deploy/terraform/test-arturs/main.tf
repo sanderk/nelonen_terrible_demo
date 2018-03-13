cffaa75829c3d6d9c8a88d58106d037a983250b8
Arturs Liepins <arturs@liepins.me>
2018-03-13 11:27:48 +0200

    Initial commit

---
+++ deploy/terraform/test-arturs/main.tf
@@ -0,0 +1,6 @@
+module "web_server" {
+    source          = "git::ssh://git@source.sanoma.com:7999/tfmod/ec2.git?ref=master"
+    name            = "webserver"
+    tags            = "${var.tags}"
+    count           = "${var.instance_count}"
+}
